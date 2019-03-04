dfd2f664ee6f8c1eb1a49b38e477f38930957c5f
Plamen <dozzerkek@hotmail.com>
2019-03-04 20:50:49 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,18 @@
 // PUERTO
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ENTORNO
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://dozzer:burberry1@cluster0-h1qyh.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
